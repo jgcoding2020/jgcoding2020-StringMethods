7bac695bf6fd53bea238a63fee60d0eec0afb274
Joshua David Gardner <jgardn11@yahoo.com>
2023-02-24 17:27:57 +0000

    Complete

--- src/main/java/SimpleStringMethods.java
+++ src/main/java/SimpleStringMethods.java
@@ -14,7 +14,8 @@ public class SimpleStringMethods {
      * @return the length of string s.
      */
     public int getStringLength(String s){
-        return 0;
+        int stringLength = s.length();
+        return stringLength;
     }
 
     /**
@@ -24,7 +25,8 @@ public class SimpleStringMethods {
      * @return the character of String s that is located at position n.
      */
     public char getCharacterAtIndex(String s, int n){
-        return ' ';
+        char character = s.charAt(n);
+        return character;
     }
 
     /**
@@ -36,7 +38,8 @@ public class SimpleStringMethods {
      * @return the index of the first occurence of c within s.
      */
     public int getIndexGivenCharacter(String s, char c){
-        return 0;
+        int cIndex = s.indexOf(c);
+        return cIndex;
     }
 
 }
